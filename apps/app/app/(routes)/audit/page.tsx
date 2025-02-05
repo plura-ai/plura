c21de032f58412047f44a7f6424718d2e8f40beb
rxhul18 <rahulshah0728@gmail.com>
2025-02-06 00:18:51 +0530

    added analytics table and dialog for detailed audit logs

--- apps/app/app/(routes)/audit/page.tsx
+++ apps/app/app/(routes)/audit/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { addDays, format } from "date-fns";
 import { CalendarIcon, RefreshCw } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -9,21 +9,33 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import { cn } from "@/lib/utils";
 import { Calendar } from '@/components/ui/calendar';
 import { DateRange } from 'react-day-picker';
+import AnalyticsTable from './analytics-table';
+
+export interface AnalyticsData {
+    action: string;
+    target: string;
+    date: string;
+}
 
 export default function page() {
-
+    const [loading, setLoading] = useState(false);
+    const [fliteredAnalyticsData, setFliteredAnalyticsData] = useState<AnalyticsData[]>([]);
     const [dateRange, setDateRange] = useState<DateRange>({
-        from: addDays(new Date(),-6),
+        from: addDays(new Date(), -6),
         to: new Date(),
     });
-    const [loading, setLoading] = useState(false);
 
-    const handleRefresh = () => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 1200);
-    }
+    const handleDateSelect = (range: DateRange | undefined) => {
+        if (range) {
+            setDateRange(range as { from: Date; to: Date });
+            if (range.from) {
+                format(new Date(range.from), "yyyy-MM-dd");
+            }
+            if (range.to) {
+                format(new Date(range.to), "yyyy-MM-dd");
+            }
+        }
+    };
 
     const analyticsData = [
         { action: "Send analytics group identify event", target: "-", date: "05 Feb 2025, 12:50:34" },
@@ -42,24 +54,35 @@ export default function page() {
         { action: "Send analytics group reset event", target: "-", date: "25 Jan 2025, 09:23:12" },
         { action: "Send analytics group identify event", target: "-", date: "22 Jan 2025, 14:35:48" },
         { action: "Send analytics group reset event", target: "-", date: "20 Jan 2025, 20:17:30" },
-      ];
-      
-      console.log(analyticsData);
-    // Logs the selected range to the console
-    const handleDateSelect = (range: DateRange | undefined) => {
-        if (range) {
-            setDateRange(range as { from: Date; to: Date });
+    ];
 
-            if (range.from) {
-                format(new Date(range.from), "yyyy-MM-dd");
-            }
+    function filterAnalyticsDataByDate({ data, dateRange }: { data: AnalyticsData[], dateRange: { from: string, to: string } }): AnalyticsData[] {
+        const fromDate = new Date(dateRange.from);
+        const toDate = new Date(dateRange.to);
+        return data.filter(item => {
+            const itemDate = new Date(item.date);
+            return itemDate >= fromDate && itemDate <= toDate;
+        });
+    }
 
-            if (range.to) {
-                format(new Date(range.to), "yyyy-MM-dd");
-            }
-        }
+    const handleRefresh = () => {
+        setLoading(true);
+        const formattedDateRange = {
+            from: dateRange.from ? format(dateRange.from, 'dd MMM yyyy, HH:mm:ss') : '',
+            to: dateRange.to ? format(addDays(dateRange.to, 1), 'dd MMM yyyy, HH:mm:ss') : ''
+        };
+
+        const fliteredAnalyticsDataRes = filterAnalyticsDataByDate({ data: analyticsData, dateRange: formattedDateRange });
+        setTimeout(() => {
+            setFliteredAnalyticsData(fliteredAnalyticsDataRes);
+            setLoading(false);
+        }, 1200);
     };
 
+    useEffect(() => {
+        handleRefresh();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
 
     return (
         <div className="container  px-5 md:px-2 text-primary rounded-lg">
@@ -72,9 +95,6 @@ export default function page() {
                 <div className="block md:flex items-center space-x-0 md:space-x-3">
                     <div className='flex items-center my-6 md:my-0 space-x-3 md:mb-0'>
                         <p className='text-muted-foreground text-sm'>Filter by</p>
-                        {/* <Button variant="outline" className="border-dashed bg-secondary text-primary">
-                            Projects
-                        </Button> */}
                         <Popover>
                             <PopoverTrigger asChild>
                                 <Button variant="outline" className="flex items-center space-x-2 bg-secondary border-dashed text-primary">
@@ -91,18 +111,29 @@ export default function page() {
                             </PopoverContent>
                         </Popover>
                     </div>
-                    <span className="text-muted-foreground text-sm">Viewing 0 logs in total</span>
+                    <span className="text-muted-foreground text-sm">Viewing {fliteredAnalyticsData?.length} logs in total</span>
                 </div>
 
-                <Button variant="default" className={`text-secondary border flex items-center space-x-2 my-6 md:my-0 ${loading?"opacity-[0.30]":""}`} onClick={handleRefresh}>
-                    <RefreshCw className={`w-4 h-4 ${loading?"animate-spin":""}`} />
+                <Button variant="default" className={`text-secondary border flex items-center space-x-2 my-6 md:my-0 ${loading ? "opacity-[0.30]" : ""}`} onClick={handleRefresh}>
+                    <RefreshCw className={`w-4 h-4 ${loading ? "animate-spin" : ""}`} />
                     <span>Refresh</span>
                 </Button>
             </div>
-
-            <Card className={cn("mt-6 border py-3 pl-5 text-start")}>
-                <p className="text-muted-foreground text-sm">You do not have any audit logs available yet</p>
-            </Card>
+            {fliteredAnalyticsData?.length > 0 ? (
+                <>
+                    <Card className={cn("mt-6 border py-3 pl-5 text-start")}>
+                        <p className="text-muted-foreground text-sm">You have audit logs available </p>
+                    </Card>
+                    <AnalyticsTable data={fliteredAnalyticsData}/>
+                </>
+            ) : (
+                <div>
+                    <Card className={cn("mt-6 border py-3 pl-5 text-start")}>
+                        <p className="text-muted-foreground text-sm">You do not have any audit logs available yet</p>
+                    </Card>
+                </div>
+            )
+            }
         </div>
     );
 }
